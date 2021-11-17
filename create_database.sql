8fd2d8075fb23c09728cf87cddcf12069dab5038
Alex Holt <alexholt54@gmail.com>
2021-11-17 15:23:07 -0800

    Moved sql into one file

--- create_database.sql
+++ create_database.sql
@@ -1,11 +1,14 @@
+-- Delete all tables
 DROP TABLE IF EXISTS Genres;
 DROP TABLE IF EXISTS Songs;
 DROP TABLE IF EXISTS Albums;
 DROP TABLE IF EXISTS Playlists;
 DROP TABLE IF EXISTS Followers;
+DROP TABLE IF EXISTS FollowersArtists;
 DROP TABLE IF EXISTS Users;
 DROP TABLE IF EXISTS Artists;
 
+-- Create tables
 CREATE TABLE Genres (
     g_key INT PRIMARY KEY,
     g_name VARCHAR(50) NOT NULL
@@ -55,3 +58,297 @@ CREATE TABLE Artists (
     ar_key INT NOT NULL,
     ar_name VARCHAR(100) NOT NULL
 );
+
+-- Populate tables
+INSERT INTO Genres
+values('1', 'HipHop');
+
+INSERT INTO Genres
+values('2', 'Pop');
+
+INSERT INTO Genres
+values('3', 'R&B');
+
+INSERT INTO Genres
+values('4', 'Electronic');
+
+INSERT INTO Genres
+values('5', 'K-Pop');
+
+INSERT INTO Genres
+values('6', 'Metal');
+
+INSERT INTO Genres
+values('7', 'Rock');
+
+INSERT INTO Genres
+values('8', 'Country');
+
+INSERT INTO Genres
+values('9', 'Alternative');
+
+INSERT INTO Genres
+values('10', 'Indie');
+
+
+
+INSERT INTO Songs
+values('1', 'Lose Yourself', '1', '1', '1');
+
+INSERT INTO Songs
+values('2', 'Like Toy Soliders', '1', '1', '1');
+
+INSERT INTO Songs
+values('3', 'Sicko Mode', '2', '1', '2');
+
+INSERT INTO Songs
+values('4', 'Back in Black', '3', '6', '3');
+
+INSERT INTO Songs
+values('5', 'Pink + White', '4', '3', '4');
+
+INSERT INTO Songs
+values('6', 'Call Out My Name', '5', '3', '5');
+
+INSERT INTO Songs
+values('7', 'StarBoy', '6', '3', '5');
+
+INSERT INTO Songs
+values('8', 'Bad Blood', '7', '2', '6');
+
+INSERT INTO Songs
+values('9', 'Bangarang', '8', '4', '7');
+
+INSERT INTO Songs
+values('10', 'Telephones', '9', '10', '8');
+
+INSERT INTO Songs
+values('11', 'Small Town Small', '10', '8', '9');
+
+INSERT INTO Songs
+values('12', 'Boys With Luv', '11', '5', '10');
+
+INSERT INTO Songs
+values('13', 'Livin on a Prayer', '12', '7', '11');
+
+INSERT INTO Songs
+values('14', 'Superstar', '13', '10', '12');
+
+
+
+INSERT INTO Albums
+values('1', 'Curtain Call - Hits(Deluxe Version)', '2005');
+
+INSERT INTO Albums
+values('2', 'AstroWorld', '2018');
+
+INSERT INTO Albums
+values('3', 'Back In Black', '1980');
+
+INSERT INTO Albums
+values('4', 'Blonde', '2016');
+
+INSERT INTO Albums
+values('5', 'My Dear Melancholy', '2018');
+
+INSERT INTO Albums
+values('6', 'Starboy', '2016');
+
+INSERT INTO Albums
+values('7', '1989', '2014');
+
+INSERT INTO Albums
+values('8', 'Bangarang', '2011');
+
+INSERT INTO Albums
+values('9', 'Changes', '2018');
+
+INSERT INTO Albums
+values('10', 'MACON', '2021');
+
+INSERT INTO Albums
+values('11', 'One Twice Melody', '2022');
+
+INSERT INTO Albums
+values('12', 'Slippery When Wet', '1986');
+
+INSERT INTO Albums
+values('13', 'MAP OF THE SOUL', '2019');
+
+
+
+INSERT INTO Artists
+values('1', 'Eminem');
+
+INSERT INTO Artists
+values('2', 'Travis Scott');
+
+INSERT INTO Artists
+values('3', 'AC/DC');
+
+INSERT INTO Artists
+values('4', 'Frank Ocean');
+
+INSERT INTO Artists
+values('5', 'The Weeknd');
+
+INSERT INTO Artists
+values('6', 'Taylor Swift');
+
+INSERT INTO Artists
+values('7', 'Skrillex');
+
+INSERT INTO Artists
+values('8', 'Vacations');
+
+INSERT INTO Artists
+values('9', 'Jason Aldean');
+
+INSERT INTO Artists
+values('10', 'BTS');
+
+INSERT INTO Artists
+values('11', 'Bon Jovi');
+
+INSERT INTO Artists
+values('12', 'Beach House');
+
+
+
+INSERT INTO Users
+values('1', 'RyanS', 'ILML');
+
+INSERT INTO Users
+values('2', 'AlexH', '#LoveWins');
+
+INSERT INTO Users
+values('3', 'TheLad', 'Xbox1');
+
+INSERT INTO Users
+values('4', 'Muska', 'Clash101');
+
+INSERT INTO Users
+values('5', 'Immortal', 'CoffeeCup');
+
+INSERT INTO Users
+values('6', 'MasterChief', 'Cortana');
+
+INSERT INTO Users
+values('7', 'JackBoy', 'Cactus');
+
+INSERT INTO Users
+values('8', 'Santosh', 'CSE20');
+
+INSERT INTO Users
+values('9', 'ImLikeThat', 'JustDoIt');
+
+INSERT INTO Users
+values('10', 'TimCook', 'Apple');
+
+
+
+INSERT INTO Followers
+values('1', '2');
+
+INSERT INTO Followers
+values('1', '10');
+
+INSERT INTO Followers
+values('1', '6');
+
+INSERT INTO Followers
+values('2', '1');
+
+INSERT INTO Followers
+values('2', '10');
+
+INSERT INTO Followers
+values('2', '8');
+
+INSERT INTO Followers
+values('2', '6');
+
+INSERT INTO Followers
+values('3', '4');
+
+INSERT INTO Followers
+values('3', '1');
+
+INSERT INTO Followers
+values('3', '6');
+
+INSERT INTO Followers
+values('4', '1');
+
+INSERT INTO Followers
+values('4', '3');
+
+INSERT INTO Followers
+values('4', '6');
+
+INSERT INTO Followers
+values('4', '8');
+
+INSERT INTO Followers
+values('5', '7');
+
+INSERT INTO Followers
+values('6', '1');
+
+INSERT INTO Followers
+values('6', '4');
+
+INSERT INTO Followers
+values('7', '2');
+
+INSERT INTO Followers
+values('7', '5');
+
+INSERT INTO Followers
+values('8', '2');
+
+INSERT INTO Followers
+values('8', '3');
+
+INSERT INTO Followers
+values('8', '4');
+
+INSERT INTO Followers
+values('9', '5');
+
+INSERT INTO Followers
+values('10', '1');
+
+
+
+
+
+INSERT INTO Playlists
+values('1', '1', '1', '0');
+
+INSERT INTO Playlists
+values('2', '2', '6', '1');
+
+INSERT INTO Playlists
+values('3', '3', '3', '1');
+
+INSERT INTO Playlists
+values('4', '4', '2', '1');
+
+INSERT INTO Playlists
+values('5', '5', '4', '1');
+
+INSERT INTO Playlists
+values('6', '6', '5', '0');
+
+INSERT INTO Playlists
+values('7', '7', '7', '0');
+
+INSERT INTO Playlists
+values('8', '8', '8', '1');
+
+INSERT INTO Playlists
+values('9', '9', '10', '0');
+
+INSERT INTO Playlists
+values('10', '10', '9', '1');
